feaaceceacdec6ec27e976089c974a2c00bb5cfe
zhumeilu <824434649@qq.com>
2021-05-26 23:54:20 +0800

    增加部分排序

--- src/main/java/com/zml/sort/HeapSort.java
+++ src/main/java/com/zml/sort/HeapSort.java
@@ -1,10 +1,49 @@
 package com.zml.sort;
 
+import java.util.Arrays;
+
 /**
  * @author: maylor
  * @date: 2021/4/23 18:45
  * @description:堆排序
  */
 public class HeapSort {
+    public static void main(String[] args) {
+        int[] arr = {7, 6, 4, 3, 5, 2, 10, 9, 8};
+        System.out.println("排序前：" + Arrays.toString(arr));
+        sort(arr);
+        System.out.println("排序后：" + Arrays.toString(arr));
+    }
 
+    public static void sort(int[] array) { // 1. 把无序数组构建成最大堆
+        for (int i = array.length / 2 - 1; i >= 0; i--) {
+            adjustHeap(array, i, array.length);
+        }// 2. 调整堆结构+交换堆顶元素与末尾元素，调整堆产生新的堆顶
+        for (int i = array.length - 1; i > 0; i--) { // 最后1个元素和第1个元素进行交换
+            int temp = array[i];
+            array[i] = array[0];
+            array[0] = temp; // “下沉”调整最大堆
+            adjustHeap(array, 0, i);
+        }
+    }
+
+    public static void adjustHeap(int[] array, int parentIndex, int length) {
+        // temp 保存父节点值，用于最后的赋值
+        int temp = array[parentIndex];
+        int childIndex = 2 * parentIndex + 1;
+        while (childIndex < length) {
+            // 如果有右孩子，且右孩子大于左孩子的值，则定位到右孩子
+            if (childIndex + 1 < length && array[childIndex + 1] > array[childIndex]) {
+                childIndex++;
+            }
+            // 如果父节点大于任何一个孩子的值，则直接跳出
+            if (temp >= array[childIndex]) break;
+            //无须真正交换，单向赋值即可
+            array[parentIndex] = array[childIndex];
+            parentIndex = childIndex;
+            //下一个左孩子
+            childIndex = 2 * childIndex + 1;
+        }
+        array[parentIndex] = temp;
+    }
 }
--- src/main/java/com/zml/sort/QuickSort.java
+++ src/main/java/com/zml/sort/QuickSort.java
@@ -1,10 +1,56 @@
 package com.zml.sort;
 
+import java.util.Arrays;
+
 /**
  * @author: maylor
  * @date: 2021/4/23 18:44
  * @description:快速排序
+ * 同冒泡排序一样，快速排序也属于交换排序，通过元素之间的比较和交换位置来达到排序的目的。
+ * 不同的是，冒泡排序在每一轮中只把1个元素冒泡到数列的一端，而快速排序则在每一轮挑选一个基准
+ * 元素，并让其他比它大的元素移动到数列一边，比它小的元素移动到数列的另一边，从而把数列拆解成
+ * 两个部分，这种思路就叫作分治法。
  */
 public class QuickSort {
 
+    public static void main(String[] args) {
+        int[] arr = new int[]{4, 7, 3, 5, 6, 2, 8, 1};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void quickSort(int[] arr, int startIndex, int endIndex) {
+        // 递归结束条件：startIndex大于或等于endIndex时
+        if (startIndex >= endIndex) {
+            return;
+        }
+        // 得到基准元素位置
+        int pivotIndex = partition(arr, startIndex, endIndex);
+        // 根据基准元素，分成两部分进行递归排序
+        quickSort(arr, startIndex, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, endIndex);
+    }
+
+    private static int partition(int[] arr, int startIndex, int endIndex) {
+        // 取第1个位置（也可以选择随机位置）的元素作为基准元素
+        int pivot = arr[startIndex];
+        int left = startIndex;
+        int right = endIndex;
+        while (left != right) { //控制right 指针比较并左移
+            while (left < right && arr[right] > pivot) {
+                right--;
+            }//控制left指针比较并右移
+            while (left < right && arr[left] <= pivot) {
+                left++;
+            }//交换left和right 指针所指向的元素
+            if (left < right) {
+                int p = arr[left];
+                arr[left] = arr[right];
+                arr[right] = p;
+            }
+        }//pivot 和指针重合点交换
+        arr[startIndex] = arr[left];
+        arr[left] = pivot;
+        return left;
+    }
 }
